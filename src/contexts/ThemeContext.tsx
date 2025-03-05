54f3a98855164b8248dd73ac512d8f0ca1aaa627
Nina Bucic <bucicnina@gmail.com>
2025-03-05 15:26:37 +0100

    Implement ThemeProvider

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,30 @@
+import { createContext, FC, ReactNode, useContext, useState } from "react";
+
+interface ThemeContextProps {
+  theme: "light" | "dark";
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
+
+export const ThemeProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = (): ThemeContextProps => {
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+  return context;
+};
